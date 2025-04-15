b106b7b5876b46b21058c452e470c79b901cd105
Alex <lexus190594@gmail.com>
2025-04-15 13:06:30 +0300

    update connection to db

--- config/db.js
+++ config/db.js
@@ -5,9 +5,6 @@ const { Pool } = pkg;
 dotenv.config();
 
 export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
